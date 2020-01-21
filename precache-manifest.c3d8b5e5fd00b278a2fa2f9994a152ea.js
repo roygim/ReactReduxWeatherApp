cb517f23e1c954af71b093d4c79f81d86355b248
roygim <roygim@gmail.com>
2020-01-21 20:55:32 +0200

    Updates

---
+++ precache-manifest.c3d8b5e5fd00b278a2fa2f9994a152ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2824e98c5841983b733f904e3b6062d",
+    "url": "/Roei-Grumet-07-01-2020/index.html"
+  },
+  {
+    "revision": "2495b58e7d7c779484c2",
+    "url": "/Roei-Grumet-07-01-2020/static/css/main.d91d6319.chunk.css"
+  },
+  {
+    "revision": "a68ea11e86c17e001015",
+    "url": "/Roei-Grumet-07-01-2020/static/js/2.52499866.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Roei-Grumet-07-01-2020/static/js/2.52499866.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2495b58e7d7c779484c2",
+    "url": "/Roei-Grumet-07-01-2020/static/js/main.4b1799ce.chunk.js"
+  },
+  {
+    "revision": "5f1b2817aea897fce888",
+    "url": "/Roei-Grumet-07-01-2020/static/js/runtime-main.5432718e.js"
+  }
+]);
