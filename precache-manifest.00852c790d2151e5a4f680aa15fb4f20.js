8e5f0d38617e525d10767f97d01ea8089b15dd04
roygim <roygim@gmail.com>
2020-01-22 19:58:28 +0200

    Updates

--- precache-manifest.00852c790d2151e5a4f680aa15fb4f20.js
+++ precache-manifest.00852c790d2151e5a4f680aa15fb4f20.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9bbe722cb73dfa92670a339dced900ce",
+    "revision": "7d001ff937cd571622d0d57fa8dda4c7",
     "url": "/Roei-Grumet-07-01-2020/index.html"
   },
   {
-    "revision": "0674542a6dbcb28df20c",
+    "revision": "624794dd5e30e8efcf27",
     "url": "/Roei-Grumet-07-01-2020/static/css/main.d91d6319.chunk.css"
   },
   {
-    "revision": "a68ea11e86c17e001015",
-    "url": "/Roei-Grumet-07-01-2020/static/js/2.52499866.chunk.js"
+    "revision": "f5e1d402a70885bf005a",
+    "url": "/Roei-Grumet-07-01-2020/static/js/2.ae494375.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/Roei-Grumet-07-01-2020/static/js/2.52499866.chunk.js.LICENSE"
+    "url": "/Roei-Grumet-07-01-2020/static/js/2.ae494375.chunk.js.LICENSE"
   },
   {
-    "revision": "0674542a6dbcb28df20c",
-    "url": "/Roei-Grumet-07-01-2020/static/js/main.bfc256c0.chunk.js"
+    "revision": "624794dd5e30e8efcf27",
+    "url": "/Roei-Grumet-07-01-2020/static/js/main.acb6ff12.chunk.js"
   },
   {
     "revision": "5f1b2817aea897fce888",
