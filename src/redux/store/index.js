96ae16158219d429552a01e28135765fb70b6d62
roygim <roygim@gmail.com>
2020-01-22 16:06:05 +0200

    changes

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -7,7 +7,7 @@ const middleware = [thunk];
 export default createStore(
     rootReducer,
     compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(...middleware)/*,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()*/
     )
 );
