852831598df6910b6c6c0572bc63b7eddea3129b
roygim <roygim@gmail.com>
2020-01-31 01:35:24 +0200

    changes

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
+const path = require( 'path' );
+
+module.exports = {
+    context: __dirname,
+    entry: './src/index.js',
+    output: {
+        path: path.resolve( __dirname, 'dist' ),
+        filename: 'main.js',
+        publicPath: '/Roei-Grumet-07-01-2020/',
+    },
+    devServer: {
+        historyApiFallback: true
+     },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_module/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css?$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.(png|j?g|svg|gif|ico)?$/,  
+                use: [{
+                    loader: 'url-loader',
+                    options: { 
+                        limit: 8000,
+                        name: 'images/[hash]-[name].[ext]'
+                    } 
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: path.resolve( __dirname, 'public/index.html' ),
+            filename: 'index.html'
+        })
+    ]
+};
